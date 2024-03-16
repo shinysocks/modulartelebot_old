970635408fb1b37acbc75ba7a2afd6950af48dbb
Noah Dinan <noah@shinysocks.net>
2024-03-15 21:54:57 -0500

    remove token

--- src/main/java/gourpbot/Main.java
+++ src/main/java/gourpbot/Main.java
@@ -1,7 +1,7 @@
 package gourpbot;
 
 public class Main {
-    private static final String TOKEN = "7080799985:AAGbSVw2p47IJzUCCtJ9uJdgeI4t1yyu68A";
+    private static final String TOKEN = "";
 
     public static void main(String[] args) {
         Bot bot = new Bot("gourpbot", TOKEN, new BotModule[] { });
