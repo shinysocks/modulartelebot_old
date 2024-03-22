795455d78ff16e24d44f25a2e3e314f6f18ab824
Michael J. Shalewski <mshalewski@outlook.com>
2024-03-22 14:27:43 -0500

    Add GitRatio Module! (#1)
    * rebased with remote repo
    
    * completed gitratio
    
    * added generated files dir to git ignore

--- src/main/java/modulartelebot/Main.java
+++ src/main/java/modulartelebot/Main.java
@@ -1,22 +1,25 @@
 package modulartelebot;
 
+import modulartelebot.botmodules.GitRatio;
+import modulartelebot.botmodules.Test;
 import io.github.cdimascio.dotenv.Dotenv;
 import modulartelebot.botmodules.KasaAutomation;
 import modulartelebot.botmodules.Pirate;
 
 public class Main {
     public static void main(String[] args) {
-        Bot bot = new Bot("gourpbot", getToken());
-
-        // add modules here:
+        Bot bot = new Bot("gourpbot", getToken("TELEGRAM_TOKEN"));
+        new Test(bot);
         new Pirate(bot);
+        new GitRatio(bot, getToken("CONVERT_API_TOKEN"));
+        // add more bots here
     }
 
-    private static String getToken() {
+    private static String getToken(String tokenKey) {
         Dotenv dotenv = Dotenv.load();
-        String token = dotenv.get("TELEGRAM_TOKEN");
+        String token = dotenv.get(tokenKey);
         if (token == null) {
-            Log.log("cannot get telegram token", Log.FLAVOR.Err);
+            Log.log("cannot get token", Log.FLAVOR.Err);
         }
         return token;
     }
---
+++ src/main/java/modulartelebot/botmodules/GitRatio.java
@@ -0,0 +1,91 @@
+package modulartelebot.botmodules;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import com.convertapi.client.Config;
+import com.convertapi.client.ConvertApi;
+import com.convertapi.client.Param;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URI;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutionException;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+
+import modulartelebot.Bot;
+import modulartelebot.BotModule;
+
+public class GitRatio extends BotModule {
+  private String[] messageArgs;
+  private final String githubChartApiURL = "https://ghchart.rshah.org/";
+  private final String convertApiURL = "https://v2.convertapi.com/convert/svg/to/png";
+  private File tempDir;
+  
+  public GitRatio(Bot bot, String convertApiToken){
+    super(bot);
+    Config.setDefaultSecret(convertApiToken);
+    setCommand("/ratio");
+  }
+
+  private File getGitActivity(URL url, String fileName) throws InterruptedException, ExecutionException, IOException{
+    File convertedImage = ConvertApi.convert("svg", "png", new Param("File", url.toString())).get().saveFile(Path.of(tempDir.getCanonicalPath())).get().toFile();
+    convertedImage.renameTo(new File(tempDir.getCanonicalPath()+"/"+fileName));
+    return convertedImage;
+  }
+
+  private void setupTempDirectory(){
+    if(tempDir == null){
+      tempDir = new File("ratio-files");
+      tempDir.mkdir();
+    }
+  }
+
+  private void deleteTempDirectory(){
+    if(tempDir != null && tempDir.exists()){
+      tempDir.delete();
+    }
+  }
+
+  @Override
+  public void update(String message, String chatId) throws TelegramApiException{
+    messageArgs = message.split(" ");
+    if(messageArgs.length < 3){
+      send(new SendMessage(chatId, "ERR: Too few arguments provided to command!"));
+    }else if(messageArgs.length > 3){
+      send(new SendMessage(chatId, "ERR: Too many arguments provided to command!"));
+    }else{
+      try {
+        setupTempDirectory();
+        SendPhoto userStatsOnePhoto = new SendPhoto();
+        userStatsOnePhoto.setChatId(chatId);
+        SendPhoto userStatsTwoPhoto = new SendPhoto();
+        userStatsTwoPhoto.setChatId(chatId);
+        URL userStatsOne = new URL(String.format("%s/%s", githubChartApiURL, messageArgs[1]));
+        File fileOne = getGitActivity(userStatsOne, "firstdev.png");
+        URL userStatsTwo = new URL(String.format("%s/%s", githubChartApiURL, messageArgs[2]));
+        File fileTwo = getGitActivity(userStatsTwo, "seconddev.png");
+        userStatsOnePhoto.setPhoto(new InputFile(tempDir.listFiles()[0]));
+        userStatsTwoPhoto.setPhoto(new InputFile(tempDir.listFiles()[1]));
+        send(new SendMessage(chatId, messageArgs[1]+"'s Git Activity"));
+        send(userStatsOnePhoto);
+        send(new SendMessage(chatId, messageArgs[2]+"'s Git Activity"));
+        send(userStatsTwoPhoto);
+        send(new SendMessage(chatId, "HAHA Soy Dev"));
+        deleteTempDirectory();
+      } catch (Exception e) {
+        System.err.println("An exception has occurred: "+e);
+      }
+    }
+  }
+
+}
